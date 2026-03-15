501776d4b447025aacb1dc6f5b7f7f7a1c0cb283
etomikadj01 <148781565+etomikadj01@users.noreply.github.com>
2026-03-15 15:11:19 +0100

    Cards and filter finished

--- javaScriptClientSide.js
+++ javaScriptClientSide.js
@@ -1,3 +1,52 @@
-$(document).ready(function(){
+/*$(document).ready(function(){
     $("#textreason").hide();
-})
+})*/
+
+function expandFilter()
+{
+    document.getElementById("dropdown").classList.toggle("show");
+}
+
+const filterButtons = document.querySelectorAll(".filterContent button");
+const filterableCards = document.querySelectorAll(".grid .card");
+
+const filterCards = e => {
+    document.querySelector(".active").classList.remove("active");
+    e.target.classList.add("active");
+    
+    filterableCards.forEach(card => {
+        card.classList.add("hide");
+
+     if(card.dataset.name === e.target.dataset.name || e.target.dataset.name === "all"){
+        card.classList.remove("hide");
+        }
+    });
+};
+
+filterButtons.forEach(button => button.addEventListener("click", filterCards));
+
+
+window.onclick = function(event)
+{
+    if (!event.target.matches('.buttonVisuals'))
+    {
+        let filtrOptions = document.getElementsByClassName("filterContent");
+        let i;
+        for (i = 0; i < filtrOptions.length; i++) {
+            var expand = filtrOptions[i];
+            if (expand.classList.contains('show')){
+                expand.classList.remove('show');
+            }
+
+        }
+    }
+} 
+
+const cards = document.querySelectorAll(".card");
+
+cards.forEach((card) => {
+    card.addEventListener("click", function()
+    {
+        this.classList.toggle("is-flipped");
+    });
+});
