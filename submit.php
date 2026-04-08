20e420d22ebf80aaf67ed411a7689e4b28815747
ZeeEms <152156488+ZeeEms@users.noreply.github.com>
2026-04-08 14:51:08 +0200

    everything works

--- submit.php
+++ submit.php
@@ -3,29 +3,31 @@
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
 
-// Connect to MySQL
+//Connect to MySQL
 $conn = new mysqli("localhost", "root", "", "malex_db");
 
-// Check connection
+//Check connection
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
 
-// Get form data
+//Get form data
 $first = $_POST['First'];
 $last = $_POST['Last'];
 $phone = $_POST['Phone'];
 $email = $_POST['email'];
 
-// Insert into database
-$sql = "INSERT INTO submissions (First, Last, Phone, Email)
-        VALUES ('$first', '$last', '$phone', '$email')";
-
-if ($conn->query($sql) === TRUE) {
-    echo "✅ Submission saved! <br><a href='view.php'>View submissions</a>";
-} else {
-    echo "Error: " . $conn->error;
+//Getting the reason stored as type
+if(isset($_POST['reason'])){
+    $reason = implode(",", $_POST['reason']);
 }
+
+
+//Insert into database
+$sql = "INSERT INTO submissions (First, Last, Phone, Email, Reason)
+        VALUES ('$first', '$last', '$phone', '$email', '$reason')";
+
+if ($conn->query($sql) === TRUE)
 
 $conn->close();
 
