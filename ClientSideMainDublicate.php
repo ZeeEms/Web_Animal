3acde883bf89cde40befcaf5b746d5dc68057ee9
etomikadj01 <148781565+etomikadj01@users.noreply.github.com>
2026-04-16 08:34:31 +0200

    Changes

--- ClientSideMainDublicate.php
+++ ClientSideMainDublicate.php
@@ -29,37 +29,9 @@
         
         <div class="grid center">
          <?php
-            include 'db.php';
-
-            $sql = 'SELECT * FROM AnimalData';
-            $result = mysqli_query($connect, $sql);
-
-            if(mysqli_num_rows($result) > 0)
-                {
-                while ($row = mysqli_fetch_assoc($result))
-                {
-                ?>
-                <div class="card" data-name="<?php echo $row['type']; ?>">
-                    <div class="cardFront">
-                        <img src="<?php echo $row['image_path']; ?>" alt="img" class="frontImg"/>
-                        <h1><?php echo $row['name']; ?></h1>
-                    </div>
-                    <div class="cardFront cardBack">
-                        <h1><?php echo $row['name']; ?></h1>
-                        <p class="cardBackPadding">
-                            <?php echo $row['description']; ?>
-                        </p>
-                        <button onclick="window.location.href='ClientSideAdoptionPage.html?adoptivePet=<?php echo urlencode($row['name']); ?>'" 
-                        class="buttonVisuals adoptBtnPlacement">Adopt</button>
-                    </div>
-                </div>
-                <?php
-                }
-            } else {
-                echo "<p>No animals for adoption :C </p>";
-            }
+           $_GET['type'] = 'all';
+           include 'FetchAnimals.php';
          ?>
-        <!---->
         </div>
     <script src="javaScriptClientSide.js"></script>        
     </body>
